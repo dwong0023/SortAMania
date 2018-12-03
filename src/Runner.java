0ec1de0b9d06fa734407f4f5e41eb99892f83008
Jacob Cedar <jcedar8640@bths.edu>
2018-12-03 11:07:36 -0500

    added strings

--- src/Runner.java
+++ src/Runner.java
@@ -4,7 +4,8 @@ public class Runner {
     {
         Team9SortCompetition sorter = new Team9SortCompetition();
         int[] testarr = {7,4,8,2,5,7,8,2,4};
-        int[][] testarr2 = {7,4,8,2,5,7,8,2,4};
+        int[][] testarr2 = {{7,4,8,2,5,7,8,2,4},{0,3,9,1,2,6,10}};
+        String[] x = {"lul","hah","memes","destruction","Bob Ross","is THE LORD"};
         System.out.println(sorter.challengeOne(testarr));
         System.out.println(sorter.challengeTwo(testarr, x));
         System.out.println(sorter.challengeThree(testarr));
