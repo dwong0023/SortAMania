355b9c1cae2275b46bebb89d94c42361c9d68257
BT_1N3_15 <dwong0023@bths.edu>
2018-12-03 11:43:28 -0500

    ethwethse

--- src/Team9SortCompetition.java
+++ src/Team9SortCompetition.java
@@ -44,7 +44,36 @@ public class Team9SortCompetition extends SortCompetition {
 
     @Override
     public int challengeThree(int[] arr) {
-        return 0;
+
+        public static void quicksort(int[] arr, int left, int right)
+        {
+            if(left < right)
+            {
+                int pivot = partition(arr,left,right);
+                quicksort(arr, left,pivot -1);
+                quicksort(arr, pivot + 1, right);
+            }
+        }
+        public static int partition(int[]arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int i = left - 1;
+            for(int j = left; j < right; j++)
+            {
+                if(arr[j] < pivot)
+                {
+                    i++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            int temp = arr[right];
+            arr[right] = arr[i+1];
+            arr[i+1] = temp;
+            return i+1;
+        }
+    return 0;
     }
 
     @Override
